d6b2922b515040b9d3111abfa2748b576eebdcc2
Hirun Mayadunna <195747264+hirunmayadunna@users.noreply.github.com>
2026-06-28 10:22:16 +0530

    Fix the student router issue

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 import express from "express"
 import mongoose from "mongoose"
-import studentRouter from "./routers/studentRouter.js";
 import userRouter from "./routers/userRouter.js";
 import authenticateUser from "./middlewares/authentication.js";
 import productRouter from "./routers/productRouter.js";
@@ -22,7 +21,6 @@ app.use(express.json())
 
 app.use(authenticateUser)
 
-app.use("/students", studentRouter)
 app.use("/users", userRouter)
 app.use("/products", productRouter)
 
